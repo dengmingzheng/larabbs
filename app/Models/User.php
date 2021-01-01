b1297f166c69281b9dd1fc3a30d8da5c438897f9
vagrant <vagrant@homestead>
2021-01-01 06:33:41 +0000

    邮箱认证

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, MustVerifyEmailTrait;
 
     /**
      * The attributes that are mass assignable.
-     *
+     * 可分配的属性
      * @var array
      */
     protected $fillable = [
@@ -24,7 +25,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * 应该为数组隐藏的属性
      * @var array
      */
     protected $hidden = [
@@ -34,7 +35,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast to native types.
-     *
+     * 应该转换类型的属性
      * @var array
      */
     protected $casts = [
